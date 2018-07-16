33bdbaf70962302ea7a909e0685fe9fd60eaf697
Hazri Jordan <hazrijordan@gmail.com>
2018-07-16 11:59:51 +0800

    Array destructuring is done

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,45 +1,70 @@
-const person = {
-    name: 'Clark',
-    age: 32,
-    location: {
-        city: 'Metropolis',
-        temp: 34
-    }
-};
+// =============================
+// OBJECT DESTRUCTURING
+// =============================
 
-// The typical way of extracting data from object.
-// console.log(`${person.name} is ${person.age} years old.`);
+// const person = {
+//     name: 'Clark',
+//     age: 32,
+//     location: {
+//         city: 'Metropolis',
+//         temp: 34
+//     }
+// };
 
-// OR 
-// const name = person.name;
-// const age = person.age;
+// // The typical way of extracting data from object.
+// // console.log(`${person.name} is ${person.age} years old.`);
 
-// console.log(`${name} is ${age} years old`);
+// // OR 
+// // const name = person.name;
+// // const age = person.age;
 
-// ES6 way of extracting.
-const { name: firstName = 'John', age } = person;
-// name = John is how we set up a default value.
-// Just in case we can't have access to values inside person, this can be the way to change it.
-// We can also combo out renaming and setting up default value.
-// Sec 10, Lec 88.
+// // console.log(`${name} is ${age} years old`);
 
-console.log(`${firstName} is ${age} years old.`);
+// // ES6 way of extracting.
+// const { name: firstName = 'John', age } = person;
+// // name = John is how we set up a default value.
+// // Just in case we can't have access to values inside person, this can be the way to change it.
+// // We can also combo out renaming and setting up default value.
+// // Sec 10, Lec 88.
 
-// Nested Objects
-const { city, temp: temperature } = person.location; 
-// temp: temperature is how you rename local variable.
-if(city && temperature) {
-    console.log(`It's ${temperature} in ${city}.`);
-};
+// console.log(`${firstName} is ${age} years old.`);
 
-const book = {
-    name: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'penguin'
-    }
-};
+// // Nested Objects
+// const { city, temp: temperature } = person.location; 
+// // temp: temperature is how you rename local variable.
+// if(city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`);
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
+// const book = {
+//     name: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'penguin'
+//     }
+// };
 
-console.log(publisherName);
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+
+// console.log(publisherName);
+
+// =============================
+// END OF OBJECT DESTRUCTURING
+// =============================
+
+// =============================
+// ARRAY DESTRUCTURING
+// =============================
+
+// const address = ['1234 Jalan Temago', 'KL', 'WP', '18954'];
+
+// const [street, city, state, zip] = address;
+// console.log(`You are in ${city} ${state}.`);
+
+// Challenge:
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [coffee, , medium] = item;
+
+console.log(`A medium ${coffee} costs ${medium}`);
