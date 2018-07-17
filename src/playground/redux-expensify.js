39eb07adfdf30be88bb144e7c541d8c73e8cdc0a
Hazri Jordan <hazrijordan@gmail.com>
2018-07-18 07:30:57 +0800

    Added UUID for ID generator and completed addExpense

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,24 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ACTIONS we need to create
 // 1. ADD_EXPENSE
+const addExpense = (
+    { 
+        description = '', 
+        note = '', 
+        amount = 0, 
+        createdAt = 0 
+    } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 // 2. REMOVE_EXPENSE
 // 3. EDIT_EXPENSE
 // 4. SET_TEXT_FILTER
@@ -14,6 +31,8 @@ import { createStore, combineReducers } from 'redux';
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return state.concat(action.expense);
         default:
             return state;
     }
@@ -42,7 +61,11 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
 
 const demoState = {
     expenses: [{
