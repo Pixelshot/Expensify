f0336f50bd4d13c5c97ab9909a801765cf49cbdf
Hazri Jordan <hazrijordan@gmail.com>
2018-07-17 16:46:35 +0800

    combineReducers(expenses & filter) is complete

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -10,6 +10,7 @@ import { createStore, combineReducers } from 'redux';
 // 7. SET_START_DATE
 // 8. SET_END_DATE
 
+// Expenses Reducer
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
@@ -18,9 +19,28 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
     }
 };
 
-// Store creation
+// Filters Reducer
 
-const store = createStore(expensesReducer);
+const filterReducerDefaultState = { 
+    text: '', 
+    sortBy: 'date', 
+    startDate: undefined, 
+    endDate: undefined 
+};
+const filterReducer = (state = filterReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Store creation
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filterReducer
+    })
+);
 
 console.log(store.getState());
 
